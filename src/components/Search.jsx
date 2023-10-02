e078052e640df780a1b3635cad24755a4788a1e7
Macphail Magwira <macphail.magwira@gmail.com>
2023-10-02 08:17:54 +0200

    Updated: removed top search bar

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -464,7 +464,7 @@ function useSearchProps() {
 }
 
 export function Search() {
- {/* let [modifierKey, setModifierKey] = useState()
+  let [modifierKey, setModifierKey] = useState()
   let { buttonProps, dialogProps } = useSearchProps()
 
   useEffect(() => {
@@ -475,7 +475,7 @@ export function Search() {
 
   return (
     <div className="hidden lg:block lg:max-w-md lg:flex-auto">
-      <button
+     {/* <button
         type="button"
         className="hidden h-8 w-full items-center gap-2 rounded-full bg-white pl-2 pr-3 text-sm text-zinc-500 ring-1 ring-zinc-900/10 transition hover:ring-zinc-900/20 dark:bg-white/5 dark:text-zinc-400 dark:ring-inset dark:ring-white/10 dark:hover:ring-white/20 lg:flex focus:[&:not(:focus-visible)]:outline-none"
         {...buttonProps}
@@ -487,9 +487,9 @@ export function Search() {
           <kbd className="font-sans">K</kbd>
         </kbd>
       </button>
-      <SearchDialog className="hidden lg:block" {...dialogProps} />
+      <SearchDialog className="hidden lg:block" {...dialogProps} />*/}
     </div>
-  )*/}
+  )
 }
 
 export function MobileSearch() {
