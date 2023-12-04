8d6a8639b751388dc79fa91f3ac790cce1c3b02e
Macphail Magwira <macphail.magwira@gmail.com>
2023-12-04 11:32:30 +0200

    Update: added manifest file

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -40,6 +40,8 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+      <link rel="manifest" href="/manifest.json"/>
+
       <link rel="preconnect" href="https://fonts.googleapis.com"/>
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
 <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Suez+One&display=swap" rel="stylesheet"/>
